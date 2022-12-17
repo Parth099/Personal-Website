add1b4b69144e6bec33a0320ffe345156a42064e
Parth099 <parthnpatel715@gmail.com>
2022-12-17 17:34:41 -0500

    cleanup project, init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                navbar: "#272727",
+                body: "#323232",
+            },
+        },
+    },
+    plugins: [],
+};
