5a86adcbdfa0f347d8e5fdb084c7a472be80d002
Parth099 <parthnpatel715@gmail.com>
2022-12-21 13:27:39 -0500

    added media queries to make skills section responsive

--- components/TechnicalSkills/SkillsList.tsx
+++ components/TechnicalSkills/SkillsList.tsx
@@ -14,11 +14,19 @@ interface SkillsListProps {
 
 export default function SkillsList({ Skills }: SkillsListProps) {
     return (
-        <div className="flex text-white items-center mb-5 xl-max:flex-col xl-max:items-start">
-            <h3 className="border-r-white border-r-4 text text-3xl font-black p-3 font-inter grow xl-max:border-0">Languages</h3>
-            <section className="grow-[2]">
-                <TechnicalSkills Skills={Skills.langs} />
-            </section>
-        </div>
+        <section className="text-white">
+            <div className="flex items-center mb-5 xl-max:flex-col xl-max:items-start">
+                <h3 className="border-r-white  text text-3xl font-black p-3 font-inter grow xl-max:border-0 lg-max:text-xl">Languages</h3>
+                <section className="grow-[2]">
+                    <TechnicalSkills Skills={Skills.langs} />
+                </section>
+            </div>
+            <div className="flex items-center mb-5 xl-max:flex-col xl-max:items-start">
+                <h3 className="border-r-white text text-3xl font-black p-3 font-inter grow xl-max:border-0 lg-max:text-xl">Frameworks/Services</h3>
+                <section className="grow-[2]">
+                    <TechnicalSkills Skills={Skills.frameworks} />
+                </section>
+            </div>
+        </section>
     );
 }
