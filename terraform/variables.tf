61889d6482d60d0870684ec06d21806ba65dec07
Parth099 <parthnpatel715@gmail.com>
2023-11-07 09:32:53 -0500

    update docker tag

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -57,7 +57,7 @@ variable "ecs_backend_container_info" {
 variable "docker_image_info" {
   type = map(string)
   default = {
-    "name" = "parth099/personal-website:0.0.2"
+    "name" = "parth099/personal-website:0.0.3"
   }
 }
 
